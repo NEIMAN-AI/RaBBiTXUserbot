2387de42bea37ca7567034c2979851bf824f2204
MrRaBBiT <darkworldxd09@gmail.com>
2023-11-18 07:55:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,3 +7,15 @@ load_dotenv()
 # API_IDS
 API_ID = int(getenv("API_ID")) # API_ID get it from my.telegram.org
 API_HASH = getenv("API_HASH") # API_HASH get it from my.telegram.org
+
+# DATABASES
+MONGO_URL = getenv("MONGO_URL", None)
+
+# SESSION
+SESSION = getenv("SESSION", None)
+
+# TOKEN
+BOT_TOKEN = getenv("BOT_TOKEN", None)
+
+# IMAGES
+ALIVE_PIC = getenv("ALIVE_PIC", None)
