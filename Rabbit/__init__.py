64eae6e1eba8632c22bfc2bd7a6db4dc4b15c6e1
MrRaBBiT <darkworldxd09@gmail.com>
2023-11-19 14:08:24 +0530

    Update __init__.py

--- Rabbit/__init__.py
+++ Rabbit/__init__.py
@@ -1,1 +1,19 @@
+import os
+import asyncio
+import sys
+import time
+from pyrogram import Client, filters
+from pytgcalls import PyTgCalls
+from config import SESSION, API_ID, API_HASH
 
+contact_filter = filters.create(
+    lambda _, __, message: (message.from_user and message.from_user.is_contact)
+    or message.outgoing
+)
+
+bot = Client(SESSION, API_ID, API_HASH, plugins=dict(root="Modules"))
+call_py = PyTgCalls(bot)
+
+
+hl = HNDLR[0]
+start_time = time.time()
